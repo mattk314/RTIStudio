d08103d41219b54a59290cd0a14e1db7b95ee5c1
mattk314 <mattk31415@gmail.com>
2019-11-25 13:15:44 -0500

    First Commit Gets courses json string well.

--- app/src/main/java/com/example/rtistudio/GetToDoTask.java
+++ app/src/main/java/com/example/rtistudio/GetToDoTask.java
@@ -13,78 +13,78 @@ import java.net.URLConnection;
 
 public class GetToDoTask extends AsyncTask<String, Void, String> {
 
-    public ToDoManager activity;
+  public ToDoManager activity;
 
-    public GetToDoTask(ToDoManager a){
-        this.activity=a;
+  public GetToDoTask(ToDoManager a){
+    this.activity=a;
+  }
+
+
+
+  @Override
+  protected String doInBackground(String... strings) {
+
+    // Create data variable for sent values to server
+
+    String data = "token=" + strings[0];
+
+    String text = "";
+    BufferedReader reader = null;
+
+    // Send data
+    try
+    {
+      // Defined URL  where to send data
+      URL url = new URL("https://rtistudio.com/api/courses");
+
+      // Send POST data request
+
+      URLConnection conn = url.openConnection();
+      conn.setDoOutput(true);
+      OutputStream outputStream = conn.getOutputStream();
+      OutputStreamWriter wr = new OutputStreamWriter(outputStream);
+      wr.write( data );
+      wr.flush();
+
+      // Get the server response
+
+      reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      StringBuilder sb = new StringBuilder();
+      String line = null;
+
+      // Read Server Response
+      while((line = reader.readLine()) != null)
+      {
+        // Append server response in string
+        sb.append(line + "\n");
+      }
+
+
+      text = sb.toString();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      try
+      {
+        reader.close();
+      }
+
+      catch(Exception ex) {
+        ex.printStackTrace();
+        text += "Exception ex 2";
+      }
     }
 
+    return text;
+  }
 
 
-    @Override
-    protected String doInBackground(String... strings) {
-
-        // Create data variable for sent values to server
-
-        String data = "token=" + strings[0];
-
-        String text = "";
-        BufferedReader reader = null;
-
-        // Send data
-        try
-        {
-            // Defined URL  where to send data
-            URL url = new URL("https://rtistudio.com/api/courses");
-
-            // Send POST data request
-
-            URLConnection conn = url.openConnection();
-            conn.setDoOutput(true);
-            OutputStream outputStream = conn.getOutputStream();
-            OutputStreamWriter wr = new OutputStreamWriter(outputStream);
-            wr.write( data );
-            wr.flush();
-
-            // Get the server response
-
-            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-
-            // Read Server Response
-            while((line = reader.readLine()) != null)
-            {
-                // Append server response in string
-                sb.append(line + "\n");
-            }
-
-
-            text = sb.toString();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                reader.close();
-            }
-
-            catch(Exception ex) {
-                ex.printStackTrace();
-                text += "Exception ex 2";
-            }
-        }
-
-        return text;
-    }
-
-
-    protected void onPostExecute(String result){
-        ToDoManager.toDoList = result;
-        Log.d("GetToDoTask returns", result);
-    }
+  protected void onPostExecute(String result){
+    ToDoManager.toDoList = result;
+    Log.d("GetToDoTask returns", result);
+  }
 }
