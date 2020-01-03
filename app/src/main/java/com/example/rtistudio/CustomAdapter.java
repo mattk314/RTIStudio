d96209b84842290af782d61cef46725a0f32cc22
mattk314 <mattk31415@gmail.com>
2020-01-03 14:26:30 -0500

    Questions JSON is received. Need to send to questionsMenu class.

---
+++ app/src/main/java/com/example/rtistudio/CustomAdapter.java
@@ -0,0 +1,66 @@
+package com.example.rtistudio;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    private String[] questions;
+    private Integer[] types;
+    private Context context;
+
+    public CustomAdapter(Context context, int textViewResourceId, Integer[] types, String[] questions) {
+        super(context, textViewResourceId, types);
+
+        this.questions = questions;
+        this.types = types;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.answer_yesno, null);
+        TextView textView = view.findViewById(R.id.questionsName);
+        textView.setText(questions[position]);
+
+        Switch yesnoAnswerSwitch = (Switch) view.findViewById(R.id.yesnoAnswerSwitch);
+        EditText answerEditText = (EditText) view.findViewById(R.id.answerEditText);
+        SeekBar answerSeekBar = (SeekBar) view.findViewById(R.id.answerSeekBar);
+
+        if(types[position]==0){
+            //text
+            yesnoAnswerSwitch.setVisibility(View.GONE);
+            answerSeekBar.setVisibility(View.GONE);
+        }
+        if(types[position]==1){
+            //slider
+            yesnoAnswerSwitch.setVisibility(View.GONE);
+            answerEditText.setVisibility(View.GONE);
+        }
+        if(types[position]==2){
+            //toggle
+            answerSeekBar.setVisibility(View.GONE);
+            answerEditText.setVisibility(View.GONE);
+        }
+
+        return view;
+    }
+
+}
+
+
+//This is the tutorial being used (maybe good maybe not): https://demonuts.com/android-custom-arrayadapter/
--- app/src/main/java/com/example/rtistudio/QuestionsMenu.java
+++ app/src/main/java/com/example/rtistudio/QuestionsMenu.java
@@ -2,9 +2,26 @@ package com.example.rtistudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class QuestionsMenu extends AppCompatActivity {
+
+    ListView questionsListView;
+    Button saveButton;
+    Button backButton;
+
+    QuestionsMenu instance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +30,60 @@ public class QuestionsMenu extends AppCompatActivity {
 
         String questionsJSON = getIntent().getExtras().getString("com.example.RTIStudio.questions");
 
+        questionsListView = findViewById(R.id.QuestionsListView);
 
- 
+        ArrayList<String> QuestionsListItems = new ArrayList<String>();
+        ArrayList<Integer> AnswerTypes = new ArrayList<Integer>();
+
+
+        //QuestionsListItems is set to an arraylist of the questions
+
+        try {
+            JSONArray questionsArray = new JSONObject(questionsJSON).getJSONArray("questions");
+
+            for(int i=0; i<questionsArray.length();i++) {
+                QuestionsListItems.add(questionsArray.getJSONObject(i).getString("question"));
+                AnswerTypes.add(questionsArray.getJSONObject(i).getInt("type"));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        CustomAdapter arrayAdapter = new CustomAdapter(this, R.layout.answer_yesno, AnswerTypes.toArray(new Integer[AnswerTypes.size()]), QuestionsListItems.toArray(new String[QuestionsListItems.size()]));
+
+
+        questionsListView.setAdapter(arrayAdapter);
+
+        backButton = (Button) findViewById(R.id.backButton);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
+        instance = this;
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Click", "Back button clicked");
+
+                //need to make this work better
+
+//                Intent i = new Intent(getApplicationContext(), ToDoManager.class);
+//                instance.startActivity(i);
+            }
+        });
+
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Click", "Save button clicked");
+
+            }
+        });
+
     }
 }
